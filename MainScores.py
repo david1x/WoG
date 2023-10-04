731f257408b3b9909a36df3939226d159b796a16
damar121 <david1.amar@intel.com>
2023-10-04 14:51:39 +0300

    fix issue that shows all the scores instead of lts

--- MainScores.py
+++ MainScores.py
@@ -9,7 +9,7 @@ def score_server():
     try:
         score_file = open(Path("Scores.txt"), "r")
         score = score_file.read()
-        return render_template('score.html', SCORE=score)  # Open score details
+        return render_template('score.html', SCORE=score[2:])  # Open score details
     except FileNotFoundError or FileExistsError:  # If the file didn't open for any reason
         return render_template('error.html', ERROR='Unknown Error')  # Prints error message
 
