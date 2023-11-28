2be36884d596ef6d777a6934e3c537f95d43edaf
David Amar <davidamar1x@gmail.com>
2023-11-28 17:42:42 +0200

    add support for linux in e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+import platform
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=Service(r"tests/chromedriver.exe"), options=options)
+
+system_os = platform.system()
+
+if system_os == 'Windows':
+    driver = webdriver.Chrome(service=Service(r"tests/chromedriver.exe"), options=options)
+else:
+    driver = webdriver.Chrome(service=Service(r"tests/chromedriver"), options=options)
+
 
 def test_scores_service(driver):
     driver.get("http://127.0.0.1:5000/")
@@ -13,7 +20,7 @@ def test_scores_service(driver):
     if 1000 >= score_element >= 1:
         return True, score_element
     else:
-        return False
+        return False, score_element
 
 
 def main_function(driver):
@@ -23,10 +30,12 @@ def main_function(driver):
     if test[0]:
         return 0, test[1]
     else:
-        return -1
+        return -1, test[1]
 
-try:
-    result = main_function(driver=driver)
-    print(f'Test Completed Successfully. Score: {result[1]}')
-except Exception as e:
-    print(f'Error: {e}')
+
+if __name__ == "__main__":
+    try:
+        result = main_function(driver=driver)
+        print(f'Test Completed Successfully. Score: {result[1]}')
+    except Exception as e:
+        print(f'Error: {e}')
