36006e082c66f49050dfb3a2e76aa05aadbac066
David Amar <davidamar1x@gmail.com>
2023-11-29 08:15:10 +0200

    Update e2e.py to support headless mode

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import platform
 
-options = webdriver.ChromeOptions()
+options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument('--headless')  # Add this line for headless mode
 
 system_os = platform.system()
 
@@ -12,7 +14,6 @@ if system_os == 'Windows':
     driver = webdriver.Chrome(service=Service(r"tests/chromedriver.exe"), options=options)
 else:
     driver = webdriver.Chrome(service=Service(r"tests/chromedriver"), options=options)
-
 
 def test_scores_service(driver):
     driver.get("http://127.0.0.1:5000/")
@@ -22,16 +23,12 @@ def test_scores_service(driver):
     else:
         return False, score_element
 
-
 def main_function(driver):
-    # The main function will return -1 as an OS exit
-    # code if the tests failed and 0 if they passed.
     test = test_scores_service(driver)
     if test[0]:
         return 0, test[1]
     else:
         return -1, test[1]
-
 
 if __name__ == "__main__":
     try:
@@ -39,3 +36,5 @@ if __name__ == "__main__":
         print(f'Test Completed Successfully. Score: {result[1]}')
     except Exception as e:
         print(f'Error: {e}')
+    finally:
+        driver.quit()  # Make sure to close the browser window
